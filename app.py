d06b8aa5121364bd91c68584e2f3cd001f861a0e
Yang Pei <pyemma1991@gmail.com>
2023-02-03 09:29:19 -0800

    deploy Heroku

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 import asyncio
 import itertools
 import json
+import os
 import secrets
+import signal
 import websockets
 
 from connect4 import PLAYER1, PLAYER2, Connect4
@@ -119,8 +121,13 @@ async def handler(websocket):
 
 
 async def main():
-  async with websockets.serve(handler, "", 8001):
-    await asyncio.Future()
+  loop = asyncio.get_running_loop()
+  stop = loop.create_future()
+  loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+  port = int(os.environ.get("PORT", "8001"))
+  async with websockets.serve(handler, "", port):
+    await stop
 
 
 if __name__ == "__main__":
